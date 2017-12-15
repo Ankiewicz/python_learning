e101e114059776159e379b3a35e8f982d1e691ea
Jonathan Ankiewicz <jankiewicz@ten-x.com>
2017-12-15 00:01:03 -0800

    adding new features, answer if not guessed, except error

--- create_list.py
+++ create_list.py
@@ -1,38 +1,40 @@
- # list to hold on to out items
-shopping_list = []
+def main():
+    # list to hold on to out items
+    shopping_list = []
 
-def show_help():
-    print("What should we ick up at the store?")
-    print("""
-    Enter 'DONE' to stop adding items.
-    Enter 'HELP' for help.
-    Enter 'SHOW' to see your current list.
-    """)
+    def show_help():
+        print("What should we ick up at the store?")
+        print("""
+        Enter 'DONE' to stop adding items.
+        Enter 'HELP' for help.
+        Enter 'SHOW' to see your current list.
+        """)
 
-def show_list():
-    print("Here is everything in the list")
+    def show_list():
+        print("Here is everything in the list")
 
-    for item in shopping_list:
-        print(item)
+        for item in shopping_list:
+            print(item)
 
-def add_to_list(new_item):
-    shopping_list.append(new_item)
-    print("Adding {}. List now has {} items".format(new_item, len(shopping_list)))
+    def add_to_list(new_item):
+        shopping_list.append(new_item)
+        print("Adding {}. List now has {} items".format(new_item, len(shopping_list)))
 
-show_help()
+    show_help()
 
-while True:
-    new_item = raw_input("> ")
+    while True:
+        new_item = raw_input("> ")
 
-    if new_item == 'DONE':
-        break
-    elif new_item == 'HELP':
-        show_help()
-        continue
-    elif new_item == 'SHOW':
-        show_list()
-        continue
-    add_to_list(new_item)
+        if new_item == 'DONE':
+            break
+        elif new_item == 'HELP':
+            show_help()
+            continue
+        elif new_item == 'SHOW':
+            show_list()
+            continue
+        add_to_list(new_item)
 
 
-show_list()
+    show_list()
+main()
---
+++ guess_it.py
@@ -0,0 +1,26 @@
+import random
+
+def game():
+    # generate a random number 1 - 10
+    secret_num = random.randint(1, 10)
+    guesses = []
+
+    while len(guesses) < 5:
+        try:
+            guess = int(raw_input("Guess a number between 1 and 10: "))
+        except ValueError:
+            print("{} isn't a number silly".format(guess))
+        else:
+            if guess == secret_num:
+                print("You got it! My number was {}".format(secret_num))
+                break
+            elif guess >= secret_num:
+                print("Lower...")
+            elif guess <= secret_num:
+                print("Higher...")
+            else:
+                print("That's not it!")
+            guesses.append(guess)
+    else:
+        print("You didn't get it! my number was {}.".format(secret_num))
+game()
