36b833685e289c89a2c07e9d85bc49d6b30453d3
Jonathan Ankiewicz <jankiewicz@ten-x.com>
2017-12-14 23:01:48 -0800

    adding features for help and show

--- create_list.py
+++ create_list.py
@@ -1,19 +1,38 @@
  # list to hold on to out items
 shopping_list = []
 
-# print out instructions
-print("What should we ick up at the store?")
-print("Enter 'DONE' to stop adding items")
+def show_help():
+    print("What should we ick up at the store?")
+    print("""
+    Enter 'DONE' to stop adding items.
+    Enter 'HELP' for help.
+    Enter 'SHOW' to see your current list.
+    """)
+
+def show_list():
+    print("Here is everything in the list")
+
+    for item in shopping_list:
+        print(item)
+
+def add_to_list(new_item):
+    shopping_list.append(new_item)
+    print("Adding {}. List now has {} items".format(new_item, len(shopping_list)))
+
+show_help()
 
 while True:
     new_item = raw_input("> ")
 
     if new_item == 'DONE':
         break
-    shopping_list.append(new_item)
+    elif new_item == 'HELP':
+        show_help()
+        continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
+    add_to_list(new_item)
 
-# be able to quit the app
-print("Here is everything in the list")
 
-# print out the list
-print(shopping_list)
+show_list()
